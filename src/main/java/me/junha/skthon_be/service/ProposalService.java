f67ec9369fbb21ff56b075b3c007758ab923ed2b
junha <wnsgk919@naver.com>
2025-09-21 03:17:52 +0900

    Update Check Proposal

--- src/main/java/me/junha/skthon_be/service/ProposalService.java
+++ src/main/java/me/junha/skthon_be/service/ProposalService.java
@@ -87,6 +87,6 @@ public class ProposalService {
 
 
     public boolean hasUserSubmittedProposal(Long userId, Long assignId) {
-        return proposalRepository.findByUserIdAndAssignId(userId, assignId).isPresent();
+        return proposalRepository.findByUserIdAndAssignment_Id(userId, assignId).isPresent();
     }
 }
