0080d3bc7313d4b0e0c77096b508dd35af66f9a3
junha <wnsgk919@naver.com>
2025-09-20 10:23:15 +0900

    DB Connect

--- src/main/java/me/junha/skthon_be/SkthonBeApplication.java
+++ src/main/java/me/junha/skthon_be/SkthonBeApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SkthonBeApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(SkthonBeApplication.class, args);
+        SpringApplication.run(SkthonBeApplication.class, args);
 	}
-
 }
