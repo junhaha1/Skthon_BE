055d9baba1fa913bbedbfc7b2670ed0d582d8088
junha <wnsgk919@naver.com>
2025-09-20 09:58:33 +0900

    frist commit

---
+++ src/main/java/me/junha/skthon_be/SkthonBeApplication.java
@@ -0,0 +1,13 @@
+package me.junha.skthon_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkthonBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkthonBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/junha/skthon_be/SkthonBeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.junha.skthon_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkthonBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
