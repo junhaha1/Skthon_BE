8a832800c25272c9bcb2c9e3e1a87276d328da85
junha <wnsgk919@naver.com>
2025-09-20 17:59:44 +0900

    Swagger Update

--- src/main/java/me/junha/skthon_be/config/SwaggerConfig.java
+++ src/main/java/me/junha/skthon_be/config/SwaggerConfig.java
@@ -2,8 +2,11 @@ package me.junha.skthon_be.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -14,6 +17,12 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("Hackathon API")
                         .description("해커톤용 로그인/회원가입 API 문서")
-                        .version("1.0.0"));
+                        .version("1.0.0"))
+                .servers(List.of(
+                        new Server().url("https://skthonbe-production.up.railway.app")
+                                .description("Railway Production 서버"),
+                        new Server().url("http://localhost:8080")
+                                .description("로컬 개발 서버")
+                ));
     }
 }
